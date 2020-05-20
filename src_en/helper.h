a7aceee08c7b13e17649d1fd538a57a761e41aed
Eugene Kopilov <64531188+Evgenk2020@users.noreply.github.com>
2020-05-20 15:49:14 +0300

    Add files via upload

---
+++ src_en/helper.h
@@ -0,0 +1,15 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <iostream>
+
+//================================================================================
+//help and error interface messages declare
+class writeln_info {
+public:
+    void help();
+    void error();
+};
+//================================================================================
+
+#endif // HELPER_H
---
+++ src_en/imtmaker.cpp
@@ -0,0 +1,135 @@
+#include "imtmaker.h"
+
+using namespace std;
+
+
+
+//================================================================================
+//class human
+void human::set_age(int temp) {age_group_bm = temp;}
+void human::set_bmi(float temp) {body_mass_index_bm = temp;}
+void human::age_group_n(string temp) {age_group_name_bm = temp;}
+void human::set_delta(float temp) {delta_bm = temp;}
+void human::set_explain(int temp) {explain_bm = temp;}
+void human::set_gender(int temp) {gender_bm = temp;}
+void human::set_interprit(string temp) {interprit_bm = temp;}
+
+void human::answer() {
+    CYAN;
+    cout << "*** BODY MASS INDEX ***" << endl << endl;
+    cout << "Gender: " << Human_Gender_Name[gender_bm] << endl;
+    cout << "Age group: " << age_group_name_bm << endl;
+    cout << "BMI: " << setprecision(4) << body_mass_index_bm << endl;
+    cout << interprit_bm;
+    if (interprit_bm != Human_Results_Name[normal]) cout << setprecision(4) << delta_bm << " kg";
+    cout << endl;
+    RESET;
+}
+//================================================================================
+
+
+//================================================================================
+//class human_maker
+void human_maker::make_new_human() {human_n = std::make_unique<human>();}
+human* human_maker::get_human(){return human_n.release();}
+
+void human_maker::activate() {
+    u_read_param = std::make_unique<read_param>();
+    u_counters = std::make_unique<counters>();
+}
+//================================================================================
+
+
+//================================================================================
+//class male_human
+void male_human::make_ager() {
+    u_counters->age_group::set_param(u_read_param->get_year(), u_read_param->get_month());
+    human_n->set_age(u_counters->age_group::get_param());
+}
+
+void male_human::make_bmi() {
+    u_counters->body_mass_index::set_param(u_read_param->get_height(), u_read_param->get_weight());
+    human_n->set_bmi(u_counters->body_mass_index::get_param());
+}
+
+void male_human::make_age_group_n() {
+    u_counters->age_grp_name::set_param(u_counters->age_group::get_param());
+    human_n->age_group_n(u_counters->age_grp_name::get_param());
+}
+
+void male_human::make_set_delta() {
+    u_counters->delta_mass::set_param(MaleBase, u_counters->body_mass_index::get_param(), u_counters->age_group::get_param(), u_read_param->get_height(), u_read_param->get_weight());
+    human_n->set_delta(u_counters->delta_mass::get_param());
+}
+
+void male_human::make_set_explain() {
+    u_counters->explain_bmi::set_param(u_counters->body_mass_index::get_param(), MaleBase, u_counters->age_group::get_param());
+    human_n->set_explain(u_counters->explain_bmi::get_param());
+}
+
+void male_human::make_set_gender() {
+    human_n->set_gender(male);
+}
+
+void male_human::make_set_interprit() {
+    u_counters->interprit_bmi::set_param(u_counters->explain_bmi::get_param());
+    human_n->set_interprit(u_counters->interprit_bmi::get_param());
+}
+//================================================================================
+
+
+
+//================================================================================
+//class female_human
+void female_human::make_ager() {
+    u_counters->age_group::set_param(u_read_param->get_year(), u_read_param->get_month());
+    human_n->set_age(u_counters->age_group::get_param());
+}
+
+void female_human::make_bmi() {
+    u_counters->body_mass_index::set_param(u_read_param->get_height(), u_read_param->get_weight());
+    human_n->set_bmi(u_counters->body_mass_index::get_param());
+}
+
+void female_human::make_age_group_n() {
+    u_counters->age_grp_name::set_param(u_counters->age_group::get_param());
+    human_n->age_group_n(u_counters->age_grp_name::get_param());
+}
+
+void female_human::make_set_delta() {
+    u_counters->delta_mass::set_param(FemaleBase, u_counters->body_mass_index::get_param(), u_counters->age_group::get_param(), u_read_param->get_height(), u_read_param->get_weight());
+    human_n->set_delta(u_counters->delta_mass::get_param());
+}
+
+void female_human::make_set_explain() {
+    u_counters->explain_bmi::set_param(u_counters->body_mass_index::get_param(), FemaleBase, u_counters->age_group::get_param());
+    human_n->set_explain(u_counters->explain_bmi::get_param());
+}
+
+void female_human::make_set_gender() {
+    human_n->set_gender(female);
+}
+
+void female_human::make_set_interprit() {
+    u_counters->interprit_bmi::set_param(u_counters->explain_bmi::get_param());
+    human_n->set_interprit(u_counters->interprit_bmi::get_param());
+}
+//================================================================================
+
+
+
+//================================================================================
+//class make_result
+void make_result::open_result() {_human_maker->get_human()->answer();}
+void make_result::create_result(human_maker *human_maker) {
+    _human_maker = human_maker;
+    _human_maker->make_new_human();
+    _human_maker->make_ager();
+    _human_maker->make_bmi();
+    _human_maker->make_age_group_n();
+    _human_maker->make_set_delta();
+    _human_maker->make_set_explain();
+    _human_maker->make_set_gender();
+    _human_maker->make_set_interprit();
+}
+//================================================================================
